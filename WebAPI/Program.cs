c33b332026be1592f4699c65b2bf45c33257a6dc
Özcan ÇAVGA <cavga1997@gmail.com>
2024-12-21 20:25:07 +0300

    Add extension method to configure SQL context in ServiceExtensions.cs.

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,4 @@
-using Microsoft.EntityFrameworkCore;
-using Repositories.EfCore;
+using WebAPI.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<RepositoryContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnection")));
+builder.Services.ConfigureSqlContext(builder.Configuration);
 
 var app = builder.Build();
 
