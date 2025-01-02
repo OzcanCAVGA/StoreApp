8a77a82344e11f7c538d65f84dc160a2739720ba
Özcan ÇAVGA <cavga1997@gmail.com>
2025-01-02 21:24:14 +0300

    nlog integrated, nlog.config created.

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,34 +1,53 @@
+﻿using NLog;
+using NLog.Web;
 using WebAPI.Extensions;
 
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers()
-    .AddApplicationPart(typeof(Presentation.AssemblyReference).Assembly)
-    .AddNewtonsoftJson();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.ConfigureSqlContext(builder.Configuration);
-builder.Services.ConfigureRepositoryManager();
-builder.Services.ConfigureServiceManager();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+var logger = LogManager.Setup().LoadConfigurationFromFile("nlog.config").GetCurrentClassLogger();
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    logger.Debug("Starting application");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+
+    // Add services to the container
+    builder.Services.AddControllers()
+        .AddApplicationPart(typeof(Presentation.AssemblyReference).Assembly)
+        .AddNewtonsoftJson();
+
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Services.ConfigureSqlContext(builder.Configuration);
+    builder.Services.ConfigureRepositoryManager();
+    builder.Services.ConfigureServiceManager();
+    builder.Services.ConfigureLoggerService();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+
+    app.MapControllers();
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+   
+    logger.Error(ex, "Application stopped because of an exception.");
+    throw;
+}
+finally
+{
+    LogManager.Shutdown();
+}
